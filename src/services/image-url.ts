bd774c3e0324f7d626df93a0143e55fe11e697d0
Feiting Ren <woamliany@gmail.com>
2024-10-11 16:57:10 -0400

    Handle image imssing

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImgae from "../assets/no-image-placeholder.webp";
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImgae;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index); // crop the url till the end of the 'media/'
